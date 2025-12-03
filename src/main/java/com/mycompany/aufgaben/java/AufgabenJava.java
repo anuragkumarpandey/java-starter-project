dbde75f431d68f1f1bf6ef43ba5e496f1691f289
Anurag <anurag11122003@gmail.com>
2025-12-03 15:28:42 +0100

    initial commit

---
+++ src/main/java/com/mycompany/aufgaben/java/AufgabenJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aufgaben.java;
+
+/**
+ *
+ * @author ap479
+ */
+public class AufgabenJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
